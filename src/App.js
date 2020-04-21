96fa011e0168cecffa26d2ca65d9e784cf82c235
Katleho <kakhany@gmail.com>
2020-04-21 05:03:34 +0200

    Async loading of components sweet.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 
-import About from './Pages/About/about';
+// import About from './Pages/About/about';
 import Contact from './Pages/Contact/contact';
 import Home from './Pages/Home/home';
-import Persons from './Pages/Persons/Persons'
+// import Persons from './Pages/Persons/Persons'
 import Person from './Pages/Persons/Person/Person';
+import NotFound from './Pages/notfound';
 import { BrowserRouter, Route, NavLink, Switch } from 'react-router-dom';
 import classes from './App.module.css';
 // import './App.module.css';
+
+const About = React.lazy(() => import('./Pages/About/about'));
+const Persons = React.lazy(() => import('./Pages/Persons/Persons'));
 
 function App() {
   return (
@@ -21,10 +25,18 @@ function App() {
         </nav>
         <Switch>
           <Route path="/" exact component={Home} />
-          <Route path="/about" exact component={About} />
+          {/* <Route path="/about" exact component={About} /> */}
+          <Route path="/about" exact render={() => <Suspense fallback={<div>Loading...</div>}><About/></Suspense>} />
           <Route path="/contact" exact component={Contact} />
-          <Route path="/person" exact component={Persons} />
+          {/* <Route path="/person" exact component={Persons} /> */}
+          <Route path="/person" exact render={() => {
+            return (
+              <Suspense fallback={<div>Loading ...</div>}>
+                <Persons />
+              </Suspense>);
+          }} />
           <Route path="/person/:id" exact component={Person} />
+          <Route component={NotFound} />
         </Switch>
       </BrowserRouter>
     </div>
